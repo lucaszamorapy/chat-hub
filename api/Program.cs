6f3a725a6411e80ab0e1ad1cbbffb6825388970a
lucaszamorapy <lucaszamora.zulmira@gmail.com>
2025-10-24 12:52:57 -0300

    Adicionar arquivos de projeto.

---
+++ api/Program.cs
@@ -0,0 +1,27 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+//scaffold "server=localhost;port=3306;database=chat;user=root;password=SENHA_AQUI" Pomelo.EntityFrameworkCore.MySql -o Models -f
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
